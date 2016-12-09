888e427c1f285a4e5c1260bf17bc68712b3c1d4a
AbbRos <abigail18itzel@gmail.com>
2016-12-09 00:57:30 -0600

    Pruebas
    Hoy hicimos pruebas con Github

---
+++ Scripts/Remplaza.py
@@ -0,0 +1,24 @@
+fileIn = open("entrada.tex","r")
+fileOut = open("salida.tex", "w")
+
+for line in fileIn:
+	print(line)
+	line = line.replace("á", "\\'a")
+	line = line.replace("é", "\\'e")
+	line = line.replace("í", "\\'i")
+	line = line.replace("ó", "\\'o")
+	line = line.replace("ú", "\\'u")
+	line = line.replace("ñ", "\\~n")
+	line = line.replace("Á", "\\'A")
+	line = line.replace("É", "\\'E")
+	line = line.replace("Í", "\\'I")
+	line = line.replace("Ó", "\\'O")
+	line = line.replace("Ú", "\\'U")
+	line = line.replace("Ñ", "\\~N")
+	line = line.replace("”", "\"")
+	line = line.replace("“", "\"")
+	line = line.replace("#", "\#")
+
+	
+	
+	fileOut.write(line)
