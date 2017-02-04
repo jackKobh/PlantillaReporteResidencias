b536533c68b24c36dafd2d8f67da8b638c83d3cd
Urbano Ballesteros Rodrigue <urbanoballesteros@gmail.com>
2017-02-04 01:45:43 -0600

    Avance
    Marco teorico add

--- Scripts/LimpiaTextoAcentos.py
+++ Scripts/LimpiaTextoAcentos.py
@@ -1,24 +1,42 @@
-fileIn = open("Introduccion.tex","r")
-fileOut = open("salida.tex", "w")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unicodedata
+import os
+import re
+fileIn = open("Introduccion.txt","r")
+fileOut = open("salida.txt", "w")
 
-for line in fileIn:
-	line = line.replace("á", "\\'a")
-	line = line.replace("é", "\\'e")
-	line = line.replace("í", "\\'i")
-	line = line.replace("ó", "\\'o")
-	line = line.replace("ú", "\\'u")
-	line = line.replace("ñ", "\\~n")
-	line = line.replace("Á", "\\'A")
-	line = line.replace("É", "\\'E")
-	line = line.replace("Í", "\\'I")
-	line = line.replace("Ó", "\\'O")
-	line = line.replace("Ú", "\\'U")
-	line = line.replace("Ñ", "\\~N")
-	line = line.replace("”", "\"")
-	line = line.replace("“", "\"")
-	line = line.replace("#", "\#")
-	line = line.replace("ú", "\\'u")
+texto = fileIn.read()
+
+# def elimina_tildes(s):
+#    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
+ 
+# print(elimina_tildes(texto))
+
+
+patron = re.compile('í+') 
+
+res = patron.sub("\\'i",texto)
+# print(res)
+fileOut.write(res)
+# for line in fileIn:
+# 	line = line.replace("á", "\\'a")
+# 	line = line.replace("é", "\\'e")
+# 	line = line.replace("í", "\\'i")
+# 	line = line.replace("ó", "\\'o")
+# 	line = line.replace("ú", "\\'u")
+# 	line = line.replace("ñ", "\\~n")
+# 	line = line.replace("Á", "\\'A")
+# 	line = line.replace("É", "\\'E")
+# 	line = line.replace("Í", "\\'I")
+# 	line = line.replace("Ó", "\\'O")
+# 	line = line.replace("Ú", "\\'U")
+# 	line = line.replace("Ñ", "\\~N")
+# 	line = line.replace("”", "\"")
+# 	line = line.replace("“", "\"")
+# 	line = line.replace("#", "\#")
+# 	line = line.replace("ú", "\\'u")
 
 	
 	
-	fileOut.write(line)
+# 	fileOut.write(line)
